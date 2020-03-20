fe464f5d54cf317ad9aade103ae43f7ea496a36e
cris luda <crisluda2244@gmail.com>
2020-03-20 15:21:26 +0000

    added class constants

---
+++ includes/classes/Constants.php
@@ -0,0 +1,19 @@
+<?php
+class Constants{
+    public static $passwordsDoNoMatch="passwors do not match";
+    public static $passwordNotAlphanumeric="password can only contain numbers and letters";
+    public static $passwordsCharacters="your password must be between 6 and 25 characters";
+    public static $emailInvalid="email is invalid";
+    public static $emailDoNoMatch="email do not match";
+    public static $lastNameCharacters="your lastname must be between 3 and 25";
+    public static $firstNameCharacters="your firstname must be between 3 and 25";
+    public static $userNameCharacters="your username must be between 6 and 25";
+}
+
+
+
+
+
+
+
+?>
