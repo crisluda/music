772cb1cbb97d1c2af63746f3170851c4d237a46f
cris luda <crisluda2244@gmail.com>
2020-03-24 09:03:21 +0000

    added the db to the config.php

---
+++ includes/config.php
@@ -0,0 +1,13 @@
+<?php
+ob_start();
+session_start();
+$timezone=date_default_timezone_set("Africa/Accra");
+$con=mysqli_connect("127.0.0.1","root","","music");
+if(mysqli_connect_errno()){
+    echo "Failed to connect: " . mysqli_connect_errno();
+}
+
+
+
+
+?>
