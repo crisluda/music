4b8842d26cca6b9c28845f3529beb6d815c7be50
cris luda <crisluda2244@gmail.com>
2020-03-24 08:57:48 +0000

    added the function if login buttom click show login and hide register button

--- includes/handlers/login-handler.php
+++ includes/handlers/login-handler.php
@@ -2,7 +2,15 @@
 
 if (isset($_POST["loginButton"])) {
     // login button was pressed
-}
+    $username=$_POST["loginUsername"];
+    $password=$_POST["loginPassword"];
+    $result=$account->login($username,$password);
+        if($result){
+            $_SESSION["userLoginedIn"]=$username;
+            header("Location:index.php");
+        }
+    }
+
 
 
 
