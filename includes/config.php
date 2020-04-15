3315aa6c690bbb258e1be9ff781dcad31658e782
crisluda <crisluda2244@gmail.com>
2020-04-15 14:26:04 +0000

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 ob_start();
 session_start();
 $timezone=date_default_timezone_set("Africa/Accra");
-$con=mysqli_connect("127.0.0.1","root","","music");
+$con=mysqli_connect("sql9.freemysqlhosting.net","sql9333608","Rg2eGM55p5","sql9333608");
 if(mysqli_connect_errno()){
     echo "Failed to connect: " . mysqli_connect_errno();
 }
